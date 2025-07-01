99bba2ca4109a0f6f097efbfa5bb3d9cb7c65751
Luke Brown <53307399+lukelbro@users.noreply.github.com>
2025-07-01 15:08:13 +0100

    Update __init__.py

--- eigenshuffle/__init__.py
+++ eigenshuffle/__init__.py
@@ -1,6 +1,6 @@
 from . import eigenshuffle_functions
-from .eigenshuffle_functions import eigenshuffle_eig, eigenshuffle_eigh, eigenshuffle_eigvals
+from .eigenshuffle_functions import eigenshuffle_eig, eigenshuffle_eigh, eigenshuffle_eighvals
 
-__all__ = ["eigenshuffle_eig", "eigenshuffle_eigh", "eigenshuffle_eigvals" ]
+__all__ = ["eigenshuffle_eig", "eigenshuffle_eigh", "eigenshuffle_eighvals" ]
 
 __all__ += eigenshuffle_functions.__all__.copy()
