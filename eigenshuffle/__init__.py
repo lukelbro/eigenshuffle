142dba1c0834b2883641538673686675d1ee79d7
Luke Brown <53307399+lukelbro@users.noreply.github.com>
2025-08-20 16:01:02 +0100

    Update __init__.py

--- eigenshuffle/__init__.py
+++ eigenshuffle/__init__.py
@@ -1,6 +1,6 @@
 from . import eigenshuffle_functions
-from .eigenshuffle_functions import eigenshuffle_eig, eigenshuffle_eigh, eigenshuffle_eighvals, eigenshuffle_eigvals
+from .eigenshuffle_functions import eigenshuffle_eig, eigenshuffle_eigh, eigenshuffle_eighvals, eigenshuffle_eigvals, eigenshuffle_eighvals_turbo
 
-__all__ = ["eigenshuffle_eig", "eigenshuffle_eigh", "eigenshuffle_eighvals", "eigenshuffle_eigvals"] 
+__all__ = ["eigenshuffle_eig", "eigenshuffle_eigh", "eigenshuffle_eighvals", "eigenshuffle_eigvals", "eigenshuffle_eighvals_turbo"]
 
 __all__ += eigenshuffle_functions.__all__.copy()
